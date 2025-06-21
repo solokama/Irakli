382fed151ee99429f518b59bc381769fab5d2fb4
solokama <solokamau9@gmail.com>
2025-06-21 16:19:25 +0300

    My First Commit

---
+++ index.js
@@ -0,0 +1,160 @@
+/* console.log("Hello World!");
+const age=30;
+
+if (age>=30 && age<40){
+    console.log("This is a gen zen");
+} else if (age > 40 && age <50) {
+
+    console.log("This is Millenial");
+} else
+
+console.log("Mmmh this is not a student");
+//variables in Javascript
+//A variable is a like a box
+
+
+
+let name = 'Zobayan';
+console.log(name);
+
+//Declare Multiple Variables
+
+let firstName ="Mosh";
+let lastName ="Hamedani";
+
+
+
+const interestRate = 0.3;
+interestRate = 1;
+console.log(interestRate);
+/* 
+
+const myInterestRateClass = 0.3;
+myInterestRateClass = 100;
+console.log(myInterestRateClass);
+ */
+
+/* //Primitives 
+
+let name = "Mosh";
+let age = 30;
+let isApproved = false;
+let isApproved = true;
+
+let firstName = "matano";
+let lastName = "Gerald";
+let selectedColor = null; */
+
+//Dynamic Language'
+//Javascript is a dynamic language
+
+
+  let person = {
+    name: "Mosh",
+    age :30,
+    isApproved:false
+};
+console.log(person);
+
+
+//.Dot Notation
+person.name ="john";
+person.age =30;
+person.language="javascript";
+console.log(person.name, person.age, person.language);
+
+
+person.name = "Porto";
+person.age = 30;
+console.log(person.name);
+console.log(person.age);
+let personn ={
+    name: "Kamaa",
+    age :40
+};
+console.log(personn);
+ 
+
+//Bracket Notation
+let selection = 'name';
+person[selection] ="ma";
+console.log(person.name);
+
+//Arrays
+let selectedColors = ['red','blue'];
+console.log(selectedColors);
+
+
+let array = ['bananas','Maize','oranges','Kales'];
+console.log(array);
+
+
+let mySubjects = ["Javascript", "C++", "Python", "Dart"];
+mySubjects[3] = 30;
+console.log(mySubjects.length);
+
+
+function greet(name, lastName, Username) {
+    console.log ('Hello' +  name + '' + lastName  + '' +Username) ;
+}
+
+greet('John','smith','Zobayan');
+greet('Mary');
+greet('solomon');
+greet('zobayan');
+
+//function that calculates a value
+
+function square(number) {
+  return number * number;
+}
+
+let number = square(6);
+console.log(number);
+
+function add(num){
+    return number + number;
+}
+
+let num =add(4);
+console.log(num);
+
+
+
+//
+
+let selectedGoon =['black', 'red','blue'];
+console.log(selectedGoon[2]);
+
+
+//function
+
+function greet(name, lastName, userName) { 
+    console.log('Hello'  +name + ' '   + lastName + ' '  + userName);
+}
+
+
+greet('John', 'Smith', 'Cute');
+
+//string primitive
+
+const message = 'This is my \n amazing taco shop';
+//string object
+const another = new String('hi');
+
+
+
+let myArray= ["clothes","pest"];
+myArray [2] = "Jackets",
+myArray[3]="Bags";
+console.log(myArray[3]);
+
+
+let ourSchools =["Julfred","Queen of Peace","Gaturiri","Mathaithi"];
+
+ourSchools[3] ="Future Leaders";
+console.log(ourSchools[3]);
+
+
+
+
